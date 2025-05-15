78596a5ec65e2de3abaf7e4dcd32c6e398b356d3
KOUYATE_Issa <161050626+MrSiuuu@users.noreply.github.com>
2025-05-15 19:04:21 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,7 @@ import { RouterView, RouterLink } from 'vue-router'
     
     <footer class="bg-white shadow-sm py-4 mt-12">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-500 text-sm">
-        <p>Application de citations © {{ new Date().getFullYear() }}</p>
+        <p>KOUYATE Issa © {{ new Date().getFullYear() }}</p>
       </div>
     </footer>
   </div>
